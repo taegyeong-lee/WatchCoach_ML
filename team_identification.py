44546805d3f02fa37a71e06e9619350104544947
TaegyeongLee <taegyeong7202@gmail.com>
2017-11-14 15:20:56 +0900

    RGB compared

--- team_identification.py
+++ team_identification.py
@@ -18,30 +18,38 @@ def image_color_cluster(image_path, k):
     return np.int_(clt.cluster_centers_)
 
 
-# @brief : 팀을 구별해주는 함수
+# @brief : 팀을 코드를 반환 해주는 함수
 # @param : RGB 색, k(색 종류)
 # @return : 1 (아군) ,-1 (적군), 0 (기타)
 
-def team_code(rgb, k):
-
+def team_code(rgb, k, standard):
+    stadium_rgb = [1, 1, 1]
+    our_team_rgb = [1, 1, 1]
+    enemy_team_rgb = [1, 1, 1]
 
     for i in range(0, k):
-        if(euclidean_distances(rgb[k], stadium_rgb) < .9):
+        if(euclidean_distances([rgb[i]], [stadium_rgb]) > standard):
             return 0;
-        if(euclidean_distances(rgb[k], our_team_rgb) < .9):
+        if(euclidean_distances([rgb[i]], [our_team_rgb]) > standard):
             return 1;
-        if(euclidean_distances(rgb[k], enemy_team_rgb) < .9):
+        if(euclidean_distances([rgb[i]], [enemy_team_rgb]) > standard):
             return -1;
 
 
-def team_division(image_path, k):
+# @brief : 팀을 구별해주는 함수 (메인)
+# @param : RGB 색, k(색 종류)
+# @return : 1 (아군) ,-1 (적군), 0 (기타)
+
+def team_division(image_path, k, standard):
     best_rgb = image_color_cluster(image_path, k)
-    code = team_code(best_rgb, k)
+    code = team_code(best_rgb, k, standard)
     return code
 
 
-
 image_path = "/Users/itaegyeong/Desktop/test2.png"
+print(team_division(image_path, 3, 500))
+
+
 
 
 
