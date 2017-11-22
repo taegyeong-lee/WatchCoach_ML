6bf693b6d8403710f415e81408d5e3600337ae18
TaegyeongLee <taegyeong7202@gmail.com>
2017-11-22 14:45:43 +0900

    team division bug fixed

--- team_identification.py
+++ team_identification.py
@@ -6,19 +6,19 @@ import cv2
 # @return : 1 (아군) ,-1 (적군), 0 (기타)
 def team_division(image):
 
-    # HSV color
-    boundaries = [
-        ([110, 51, 51],[130, 255, 255])
-
-
-    ]
-
     # image load
-    image_path = "/Users/itaegyeong/Desktop/testblue.png"
+    image_path = "/Users/itaegyeong/Desktop/testblue2.png"
     img = cv2.imread(image_path) # or image
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    for (lower, upper) in boundaries:
+    # HSV color
+    boundaries = [
+        ([0],[0,0,0],[1,1,1]), # 기타
+        ([1],[110, 51, 51],[130, 255, 255]) # 아군
+
+    ]
+
+    for (code, lower, upper) in boundaries:
         lower = np.array(lower, dtype='uint8')
         upper = np.array(upper, dtype='uint8')
         mask = cv2.inRange(img_hsv, lower, upper)
@@ -29,7 +29,6 @@ def team_division(image):
                 if j == 255:
                     count = count + 1
 
-    if count > 10000:
-
-
+        if count > 10000:
+            return code[0]
 
