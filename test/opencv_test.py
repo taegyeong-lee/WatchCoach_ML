1ca8d8ca1b01197770bedadf815c888dd31299ae
TaegyeongLee <taegyeong7202@gmail.com>
2017-11-28 18:53:40 +0900

    threshold fixing

--- test/opencv_test.py
+++ test/opencv_test.py
@@ -18,7 +18,7 @@ def morphology(frame):
 
 
 # 움직이는 물체 탐지해서 이미지로 바꿔서 반환
-def moving_object(frame):
+def moving_mask(frame):
     fg_mask = mog.apply(frame)
     fg_mask2 = cv2.medianBlur(fg_mask, 5)
     final_img = cv2.bitwise_and(frame, frame, mask=fg_mask2)
@@ -39,13 +39,11 @@ def color_detection(frame, rgb_lower, rgb_upper, teamcode):
 def contours_emphasis(frame, mask, fill):
     copy = frame.copy()
     _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
     for i in contours:
         x, y, w, h = cv2.boundingRect(i)
 
         if w<10 or h <10:
             continue
-
         cv2.rectangle(copy, (x, y), (x + w, y + h), (0, 0, 255), fill)
 
     return copy
@@ -55,19 +53,39 @@ while True:
     ret, frame = video.read()
 
     # 움직이는 물체 감지
-    moving_mask = moving_object(frame)
+    moving_frame = moving_mask(frame)
 
     # 붉은색 물체 감지
     red_detection_mask = color_detection(frame, [150, 150, 50], [190, 255, 255], 1)
-    contours_emphasis_frame = contours_emphasis(frame, red_detection_mask, -1)
 
-    red_detection_mask2 = color_detection(contours_emphasis_frame, [0, 254, 254], [2, 255, 255], 1)
-    contours_emphasis_frame = contours_emphasis(frame, red_detection_mask2, 2)
+    # 2진화 진화
+    ret, thresh1 = cv2.threshold(red_detection_mask, 127, 255, cv2.THRESH_BINARY)
+    ret, thresh2 = cv2.threshold(red_detection_mask, 127, 255, cv2.THRESH_BINARY_INV)
+    ret, thresh3 = cv2.threshold(red_detection_mask, 127, 255, cv2.THRESH_TRUNC)
+    ret, thresh4 = cv2.threshold(red_detection_mask, 127, 255, cv2.THRESH_TOZERO)
+    ret, thresh5 = cv2.threshold(red_detection_mask, 127, 255, cv2.THRESH_TOZERO_INV)
 
+    cv2.imshow('moving_frame', moving_frame)
+
+    cv2.imshow('thresh1', thresh1)
+    cv2.imshow('thresh2', thresh2)
+    cv2.imshow('thresh3', thresh3)
+    cv2.imshow('thresh4', thresh4)
+    cv2.imshow('thresh5', thresh5)
+
+
+    th2 = cv2.adaptiveThreshold(thresh1, 255, cv2.ADAPTIVE_THRESH_MEAN_C, \
+                                cv2.THRESH_BINARY, 11, 2)
+    th3 = cv2.adaptiveThreshold(thresh1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+                                cv2.THRESH_BINARY, 11, 2)
+
+    cv2.imshow('a', th3)
+    cv2.imshow('b', th2)
 
 
     cv2.imshow('red_mask1', red_detection_mask)
-    cv2.imshow('contours_emphasis_mask', contours_emphasis_frame)
 
 
-    cv2.waitKey(1)
+
+
+    cv2.waitKey(0)
