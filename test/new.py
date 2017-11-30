fe2b099812bbf85def824404614dfaa7eae63305
TaegyeongLee <taegyeong7202@gmail.com>
2017-11-30 11:45:47 +0900

    morphology testing

--- test/new.py
+++ test/new.py
@@ -13,9 +13,12 @@ def histogram(img):
 
 video = cv2.VideoCapture('/Users/itaegyeong/Desktop/good.mov')
 knn = cv2.createBackgroundSubtractorKNN()
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('test_basketball_output.mp4',fourcc, 20.0, (640,360))
 
 while True:
     ret, frame = video.read()
+    cv2.imshow('frame',frame)
 
     # Morphology의 opening, closing을 통해서 노이즈나 Hole제거
     kernel = np.ones((3, 3), np.uint8)
@@ -28,16 +31,16 @@ while True:
     mask = cv2.inRange(opening_hsv, np.array((150., 150., 50.)), np.array((190., 255., 255.)))
     cv2.imshow('masks', mask)
 
-    opening2 = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=3)
+    opening2 = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
     cv2.imshow('opening2 mask',opening2)
 
-    _, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    _, contours, _ = cv2.findContours(opening2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for c in contours:
         x, y, w, h = cv2.boundingRect(c)
 
         area = w * h
-        if area < 200 or y < 40:
-            continue
+        #if area < 200 or y < 40:
+        #    continue
 
 
         print(x, y, area)
@@ -46,7 +49,9 @@ while True:
 
 
     cv2.imshow('frame',frame)
+    out.write(frame)
 
 
-    cv2.waitKey(1)
+
+    cv2.waitKey(0)
 
