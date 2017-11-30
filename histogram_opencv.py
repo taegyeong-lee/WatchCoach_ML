51a8f0604d07a7984de3f0433b947725a3b719e5
TaegyeongLee <taegyeong7202@gmail.com>
2017-11-30 11:54:38 +0900

    morphology testing

--- histogram_opencv.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-
-
-
-
-
--- opencv_main.py
+++ opencv_main.py
@@ -1,19 +1,10 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
-
-
-def histogram(img):
-    hist = cv2.calcHist([img],[2],None,[256],[0,256])
-    plt.plot(hist, color='r')
-    plt.xlim([0,256])
-    plt.show()
 
 
 video = cv2.VideoCapture('/Users/itaegyeong/Desktop/good.mov')
 knn = cv2.createBackgroundSubtractorKNN()
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('test_basketball_output.mp4',fourcc, 20.0, (640,360))
+
 
 while True:
     ret, frame = video.read()
@@ -24,11 +15,11 @@ while True:
     opening = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel, iterations=1)
     cv2.imshow('opening',opening)
 
-
     opening_hsv = cv2.cvtColor(opening,cv2.COLOR_BGR2HSV)
 
     mask = cv2.inRange(opening_hsv, np.array((150., 150., 50.)), np.array((190., 255., 255.)))
     cv2.imshow('masks', mask)
+
 
     opening2 = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
     cv2.imshow('opening2 mask',opening2)
@@ -46,11 +37,6 @@ while True:
 
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-
     cv2.imshow('frame',frame)
-    out.write(frame)
-
-
-
     cv2.waitKey(0)
 
