7a595bc71275447247fc6da24d520ab60ba227f5
TaegyeongLee <luckperson7@naver.com>
2017-12-09 20:01:03 +0900

    transfer_view

--- raspberrypi/draw_canvas.py
+++ raspberrypi/draw_canvas.py
@@ -1,31 +1,44 @@
 import numpy as np
 import cv2
-import random
 
-# 407 × 720
 
-def canvas_show(our_team_point, enemy_team_point, other_point, w, h):
-
+def standard_point(point, w, h):
     weight = 407/w
     height = 720/h
 
-    for i in our_team_point:
-        print("i",i)
+    for i in point:
+        for j in i:
+            j[0] = int(j[0] * weight)
+            j[1] = int(j[1] * height)
 
+    return point
+
+
+# 407 × 720
+def canvas_show(our_team_point, enemy_team_point, other_point, w, h):
 
     background_img = cv2.imread('/Users/itaegyeong/Desktop/background.png') # 407x720
     cv2.imshow('back', background_img)
 
-    draw_circle(background_img, our_team_point, (255, 0, 0))
-    draw_circle(background_img, enemy_team_point, (0, 0, 255))
-    draw_circle(background_img, other_point, (0, 255, 0))
+    if our_team_point != []:
+        print(our_team_point)
+        our_team_point = standard_point(our_team_point,w,h)
+        print(our_team_point)
+        draw_circle(background_img, our_team_point, (255, 0, 0))
 
+    if enemy_team_point != []:
+        enemy_team_point = standard_point(enemy_team_point, w, h)
+        draw_circle(background_img, enemy_team_point, (0, 0, 255))
 
+    if other_point != []:
+        other_point = standard_point(other_point, w, h)
+        draw_circle(background_img, other_point, (0, 255, 0))
+
+    cv2.imshow('back',background_img)
     cv2.waitKey(1)
 
 
 def draw_circle(image, point, rgb):
     for i in point:
-            cv2.circle(image, (tuple)(i), 3, rgb, -1)
-
-
+        for j in i:
+            cv2.circle(image, (tuple)(j), 7, rgb, -1)
--- raspberrypi/get_object_point.py
+++ raspberrypi/get_object_point.py
@@ -1,8 +1,7 @@
 import cv2
 import numpy as np
-
 from raspberrypi import get_stadium_point as gsp # Get stadium's points
-from raspberrypi import draw_canvas as dp # Draw camvas top view
+from raspberrypi import draw_canvas as dc # Draw camvas top view
 from raspberrypi import get_object_point as gop # Get object's points
 from raspberrypi import transfer_view as gtv # Transfer object's points
 
@@ -31,6 +30,9 @@ def team_division(frame):
         _, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         for c in contours:
             x, y, w, h = cv2.boundingRect(c)
+            center_x = x+(1/2)*w
+            center_y = y + h
+
 
             area = w * h
             if area < 500:
@@ -39,18 +41,18 @@ def team_division(frame):
             # enemy team
             if code == -1:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                enemy_team_point.append([x,y])
+                enemy_team_point.append([center_x,center_y])
             # our team
             elif code == 1:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                our_team_point.append([x, y])
+                our_team_point.append([center_x, center_y])
             # other team
             elif code == 0:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                other_point.append([x,y])
+                other_point.append([center_x,center_y])
 
     cv2.imshow('frame',frame)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
     return our_team_point,enemy_team_point,other_point
 
 
@@ -66,28 +68,38 @@ def test():
     cap = cv2.VideoCapture('/Users/itaegyeong/Desktop/GOPR0011.mov')
 
     # Get trans Matrix, trans image weight and height
-    trans_matrix, trans_w, trans_h = gtv.get_trans_matrix([206,47], [512,149], [65, 159],
-                                                          [421,332])
+    trans_matrix, trans_w, trans_h = gtv.get_trans_matrix([31, 160], [424, 346], [206, 38],
+                                                          [526, 147])
 
     if cap.isOpened():
+
         while True:
             ret, frame = cap.read()
 
-            # Morphology의 opening, closing을 통해서 노이즈제거
-            kernel = np.ones((3, 3), np.uint8)
-            opening = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel, iterations=1)
-            cv2.imshow('opening', opening)
+            if ret is True:
 
-            our_team_point, enemy_team_point, other_point = team_division(opening)
+                # Morphology의 opening, closing을 통해서 노이즈제거
+                kernel = np.ones((3, 3), np.uint8)
+                opening = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel, iterations=1)
 
-            # Get trans object's points
-            dst, trans_our_team_point, trans_enemy_team_point, trans_other_point = \
-                gtv.trans_object_point(frame, our_team_point, enemy_team_point,other_point, trans_matrix, trans_w, trans_h)
-            gtv.trans_object_point(frame, our_team_point, enemy_team_point, other_point, trans_matrix, trans_w, trans_h)
+                our_team_point, enemy_team_point, other_point = team_division(opening)
 
-            cv2.imshow('dst',dst)
+                # Get trans object's points
+                dst, trans_our_team_point, trans_enemy_team_point, trans_other_point = \
+                    gtv.trans_object_point(opening, our_team_point, enemy_team_point,other_point, trans_matrix, trans_w, trans_h)
 
+                if trans_our_team_point != []:
+                    dc.draw_circle(dst,trans_our_team_point,(255, 0, 0))
+                if trans_enemy_team_point != []:
+                    dc.draw_circle(dst,trans_enemy_team_point,(0, 0, 255))
+                if trans_other_point != []:
+                    dc.draw_circle(dst,trans_other_point,(0, 255, 0))
 
-            result_frame = dp.canvas_show(our_team_point, enemy_team_point, our_team_point, trans_w, trans_h)
+                cv2.imshow('dst',dst)
+                cv2.waitKey(0)
+
+                result_frame = dc.canvas_show(trans_our_team_point, trans_enemy_team_point, trans_other_point, trans_w, trans_h)
+
+        cv2.destroyAllWindows()
 
 test()
